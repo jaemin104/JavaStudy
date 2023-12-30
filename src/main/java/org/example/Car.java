78dcf0112536fd90bb3994c10206eb64be579fc6
jaemin104 <kjm041107@naver.com>
2023-12-30 22:36:07 +0900

    Car 클래스

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,56 @@
+package org.example;
+/*한줄복사 커맨드d
+생성자 게터세터 커맨드n
+실행 컨트롤r
+퀵픽스 옵션엔터
+한줄삭제, 맥 롬복 설치 방법 인식이 안됨
+ */
+
+public class Car {
+    private String name;
+    private int price;
+    private String brand;
+
+    public Car() {}
+
+    public Car(String name, int price, String brand) {
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void printInfo() {
+        System.out.println("차 이름: "+name);
+        System.out.println("차 가격: "+price);
+        System.out.println("차 브랜드: "+brand);
+    }
+
+    public String printInfo2() {
+        return "차 이름: "+name+", 차 가격: "+price;
+    }
+}
+
---
+++ src/main/java/org/example/MainCar.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class MainCar {
+    public static void main(String[] args) {
+        Car car1 = new Car("sonata", 100, "hyundai");
+        System.out.println(car1.getName()+", "+car1.getPrice()+", "+car1.getBrand());
+        car1.printInfo();
+        String info2 = car1.printInfo2();
+        System.out.println(info2);
+
+        Car car2 = new Car("520d", 200, "BMW");
+        car2.setPrice(300);
+        car2.printInfo();
+        String s = car2.printInfo2();
+        System.out.println(s);
+
+        Car car3 = new Car();
+
+    }
+
+}
