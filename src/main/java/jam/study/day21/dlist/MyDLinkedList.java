8529ac2ae39c0834f57d2982a28c774666d84217
jaemin104 <kjm041107@naver.com>
2024-02-16 01:34:41 +0900

    노드 삽입, 삭제 기능 추가

--- src/main/java/jam/study/day21/dlist/MyDLinkedList.java
+++ src/main/java/jam/study/day21/dlist/MyDLinkedList.java
@@ -1,4 +1,78 @@
 package jam.study.day21.dlist;
 
 public class MyDLinkedList {
+    private DLNode DL = null;
+    public void insertNode(String data) {
+
+        DLNode newNode = new DLNode(data);
+
+        if (DL == null) {  // 처음 넣는 경우
+            DL = newNode;
+        } else {  // 기존에 있는데 넣는 경우
+            DLNode temp = DL;
+            while (temp.getRlink() != null) {
+                temp = temp.getRlink();
+            }
+            DLNode pre = temp;
+            pre.setRlink(newNode);
+            newNode.setLlink(pre);
+        }
+
+
+    }
+
+    public void printDList() {
+        DLNode temp = DL;
+        while (temp != null) {
+            System.out.print(temp.getData() + " ");
+            temp = temp.getRlink();
+        }
+        System.out.println();
+    }
+
+    public DLNode searchNode(String data) {
+        DLNode temp = DL;
+        DLNode pre = null;
+
+        while (temp != null) {
+            if (temp.getData() == data) {
+                pre = temp;
+                break;
+            }
+            temp = temp.getRlink();
+        }
+        return pre;
+    }
+
+    public void insertMiddleNode(DLNode pre, String data) {
+        DLNode newNode = new DLNode(data);
+
+        if (DL == null){
+            DL = newNode;
+        } else {
+            newNode.setRlink(pre.getRlink());
+            pre.getRlink().setLlink(newNode);
+            pre.setRlink(newNode);
+            newNode.setLlink(pre);
+        }
+
+    }
+
+    public void insertFirstNode(String data) {
+        DLNode newNode = new DLNode(data);
+        newNode.setRlink(DL);
+        DL.setLlink(newNode);
+        DL = newNode;
+    }
+
+    public void deleteNode(DLNode deleteNode) {
+        if (deleteNode == DL) {
+            DL = deleteNode.getRlink();
+        } else {
+            DLNode pre = deleteNode.getLlink();
+            DLNode post = deleteNode.getRlink();
+            post.setLlink(pre);
+            pre.setRlink(deleteNode.getRlink());
+        }
+    }
 }
--- src/main/java/jam/study/day21/dlist/MyDLinkedListMain.java
+++ src/main/java/jam/study/day21/dlist/MyDLinkedListMain.java
@@ -1,44 +1,32 @@
 package jam.study.day21.dlist;
 
+import java.util.LinkedList;
+
 public class MyDLinkedListMain {
     public static void main(String[] args) {
+
         System.out.println("(1) 공백 리스트에 노드 3개 삽입하기");
         MyDLinkedList week = new MyDLinkedList();
 
-        week.insert
+        week.insertNode("월");
+        week.insertNode("화");
+        week.insertNode("수");
+        week.printDList();
+
+        System.out.println("(2) 화 노드 뒤에 화.5 노드 삽입하기");
+        DLNode pre = week.searchNode("화");
+        System.out.println("pre : " + pre.getData());
+        week.insertMiddleNode(pre, "화.5");
+        week.printDList();
+
+        System.out.println("(3) 첫번째 위치에 '시작'이라는 단어를 삽입하세요.");
+        week.insertFirstNode("시작");
+        week.printDList();
+
+        System.out.println("(4) 시작 노드를 삭제하세요.");
+        DLNode deleteNode = week.searchNode("시작");
+        week.deleteNode(deleteNode);
+        week.printDList();
     }
-//    MyLinkedList list = new MyLinkedList();
-//        System.out.println("(1) 공백 리스트에 노드 3개 삽입하기");
-//        list.insertLastNode("월");
-//        list.printList();
-//    ListNode deleteNode = list.searchNode("월");
-//        list.deleteNode(deleteNode);
-//        list.printList();
-//        list.insertLastNode("수");
-//        list.insertLastNode("일");
-//        list.printList();
-//
-//        System.out.println("(2) 수 노드 뒤에 금 노드 삽입하기");
-//    ListNode pre = list.searchNode("수");
-//        System.out.println(pre.day);
-//
-//        if (pre == null) {
-//        System.out.println("검색 실패");
-//    } else {
-//        list.insertMiddleNode(pre, "금");
-//        list.printList();
-//    }
-//
-//        System.out.println("(3) 첫번째 위치에 '시작'이라는 단어를 삽입하세요.");
-//        list.insertFirstNode("시작");
-//        list.printList();
-//
-//        System.out.println("(4) 시작 노드를 삭제하세요.");
-//    deleteNode = list.searchNode("시작");
-//        list.deleteNode(deleteNode);
-//        list.printList();
-//
-//        System.out.println("(5) 거꾸로 출력하세요.");
-//        list.reverse();
-//        list.printList();
+
 }
